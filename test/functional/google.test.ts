9d9eb4380b0aecc40536076a574cfa72f55ac862
Picosu <maxence.decussac@gmail.com>
2019-09-22 16:06:04 +0200

    rajout d'enum mais pb avec test:watch

--- test/functional/google.test.ts
+++ test/functional/google.test.ts
@@ -2,14 +2,36 @@ import supertest, { Response } from 'supertest'
 import { request } from 'http'
 import { emptyStatement } from '@babel/types'
 
-jest.setTimeout(1000)
+jest.setTimeout(2000)
 const BASE_URL = "http://localhost:3000";
 
+/// API Values 
+enum Api {
+  v5 = '/api/v5.3',
+  v6 = '/api/v6'
+}
+
+/// Domain for Preproduction server
+enum Preprod {
+  akamaiPrp = 'https://pass-prp.louisvuitton.com',
+  herokuPrp = 'https://lvpass-prp.herokuapp.com'
+}
+
+/// Domain for Production server
+enum Production {
+  akamai = 'https://pass.louisvuitton.com',
+  heroku = 'https://lv-lvpassv5-prp.herokuapp.com'
+}
+
+export const Environment = { ...Production, ...Preprod };
+export type Environment = typeof Environment;
+
 describe('Preprod internationale test', () => {
-  var domain = 'https://lvpass-prp.herokuapp.com'
-  var prodTest = 'https://lv-lvpass18-prd.herokuapp.com'
-  var currentLocale = 'eng_E1'
-  var headers =
+  const domainV5 = `${Environment.akamaiPrp}${Api.v5}`
+  const domainV6 = `${Environment.akamaiPrp}${Api.v6}`
+
+  const currentLocale = 'eng_E1'
+  const headers =
   {
     'x-wsse': 'true',
     'User-Agent': 'LVapp/android/5.4.6 LVApp PRD'
@@ -18,7 +40,8 @@ describe('Preprod internationale test', () => {
   const myReq = require('supertest')
 
   describe('SyncVersion', () => {
-    var currentUrl = domain + '/api/v5.3/syncVersion?locale=' + currentLocale
+    const currentUrl = `${domainV5}/syncVersion?locale=${currentLocale}`
+
     it('should return 200 on get', async () => {
       const res: Response = await supertest('').get(currentUrl)
       expect(res.status).toBe(200)
@@ -31,7 +54,8 @@ describe('Preprod internationale test', () => {
 
 
   describe('Sync', () => {
-    var currentUrl = domain + '/api/v5.3/sync?locale=' + currentLocale
+    const currentUrl = `${domainV5}/sync?locale=${currentLocale}`
+
     it('should return 200 on get', async () => {
       const res: Response = await supertest('').get(currentUrl).set(headers)
     })
@@ -42,7 +66,8 @@ describe('Preprod internationale test', () => {
   })
 
   describe('Web-cache-catalog', () => {
-    var currentUrl = `${domain}/api/v5.3/website-cache-catalog?locale=${currentLocale}`
+    const currentUrl = `${domainV5}/api/v5.3/website-cache-catalog?locale=${currentLocale}`
+
     it('should return 200 on get', async () => {
       const res: Response = await supertest('').get(currentUrl).set(headers)
     })
@@ -54,8 +79,9 @@ describe('Preprod internationale test', () => {
 
 
   describe('Detail-catalog', () => {
-    var firstUrl = domain + '/api/v5.3/website-cache-catalog?locale=' + currentLocale
-    var currentUrl = domain + '/api/v5.3/detail-catalog?locale=' + currentLocale
+    const firstUrl = `${domainV5}/website-cache-catalog?locale=${currentLocale}`
+    const currentUrl = `${domainV5}/detail-catalog?locale=${currentLocale}`
+
     it('should return 200 on get', async () => {
       const get: Response = await supertest('').get(firstUrl).set(headers)
 
@@ -63,7 +89,7 @@ describe('Preprod internationale test', () => {
       var userId = get.body.femme.contents[0].contents[0].id
 
       var detailUrl = currentUrl + '&id=' + userId
-      const res: Response = await supertest('').get(detailUrl).set('x-wsse','true').set('User-Agent','LVapp/android/5.4 LVApp PRD')
+      const res: Response = await supertest('').get(detailUrl).set('x-wsse', 'true').set('User-Agent', 'LVapp/android/5.4 LVApp PRD')
 
       expect(res.status).toBe(200)
     })
@@ -84,7 +110,7 @@ describe('Preprod internationale test', () => {
   }); */
 
   describe('Auto Complete', () => {
-    var currentUrl = `${domain}/api/v6/geocodings/autocomplete?input="rue"&country=FR&locale=${currentLocale}`
+    const currentUrl = `${domainV6}/geocodings/autocomplete?input="rue"&country=FR&locale=${currentLocale}`
     it('should return 200 on get', async () => {
       const res: Response = await supertest('').get(currentUrl)
       console.log(currentUrl)
@@ -97,23 +123,25 @@ describe('Preprod internationale test', () => {
   })
 
   describe('SaveLook', () => {
-    var currentUrl = domain + '/api/v6/saveLook'
+    const currentUrl = `${domainV6}/saveLook`
+
     it('should return 405 on get', async () => {
       const res: Response = await supertest('').get(currentUrl)
       expect(res.status).toBe(405)
     })
     it('should return 200 on post', async () => {
-      const res: Response = await supertest('').post(currentUrl).send({ "salesforceUserId": '00555000003bbe4AAA','':'' }) // 00555000003bbe4AAA => mylvtest9@dispostable.com
+      const res: Response = await supertest('').post(currentUrl).send({ "salesforceUserId": '00555000003bbe4AAA', 'lookId': 'a2E55000000b5ldEAA' }) // 00555000003bbe4AAA => mylvtest9@dispostable.com
       expect(res.status).toBe(200)
     })
     it('should not return 200 on post', async () => {
-      const res: Response = await supertest('').post(currentUrl).send({ "salesforceUserId": '00555000003bbe4AAA','':'' }) // 00555000003bbe4AAA => mylvtest9@dispostable.com
+      const res: Response = await supertest('').post(currentUrl).send({ "salesforceUserId": '00555000003bbe4AAA', '': '' }) // 00555000003bbe4AAA => mylvtest9@dispostable.com
       expect(res.status).not.toBe(200)
     })
   })
 
   describe('Wishlist', () => {
-    var currentUrl = domain + '/api/v5.3/wishlists/custom?salesforce_id=' + '00555000003bbe4AAA'
+    var currentUrl = `${domainV5}/wishlists/custom?salesforce_id=00555000003bbe4AAA`
+
     it('should return 200 on get', async () => {
       const res: Response = await supertest('').get(currentUrl).set(headers)
       expect(res.body.success).toBe(true)
